05802c25d1ee1a916282cec2cb0f536962293068
root <root@linux-centos7-01.fios-router.home>
2018-02-26 09:32:05 -0500

    new install

---
+++ wp-config.php
@@ -0,0 +1,9 @@
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('DB_NAME', 'wordpress');
+/** MySQL database username */
+define('DB_USER', 'dtbui');
+/** MySQL database password */
+define('DB_PASSWORD', 'testing123!');
+/** MySQL hostname */
+define('DB_HOST', 'localhost');
